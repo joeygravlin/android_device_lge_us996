d3eb62d2ba6c5180238ce50be4d68119044e394d
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-02-06 01:52:43 +0000

    Disabled offset since moved second screen fix to kernel, and added new kernel build
    Change-Id: I8ea3807b232f2b8b5e946f7589fa99b2ed5a9ca4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CONFIG := twrp_defconfig
 TARGET_KERNEL_DEVICE_DEFCONFIG := device_lge_us996
 
-TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.lz4-dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image-us996.lz4-dtb
 
 # Boot image
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.hardware=elsa androidboot.bootdevice=624000.ufshc androidboot.selinux=permissive
@@ -71,8 +71,11 @@ TW_INCLUDE_NTFS_3G := true
 
 TW_IGNORE_MISC_WIPE_DATA := true
 # Shift TWRP off the secondary screen
-TW_Y_OFFSET := 160
-TW_H_OFFSET := -160
+#top
+#TW_Y_OFFSET := 35
+
+#Bottom
+#TW_H_OFFSET := -35
 
 # exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
